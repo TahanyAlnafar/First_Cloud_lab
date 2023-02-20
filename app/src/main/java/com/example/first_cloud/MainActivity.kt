72e615ad3dbdc690d202e66d8a6091789eabaa09
TahanyAlnafar <tahanyalnfar@gmail.com>
2023-02-20 11:30:01 +0200

    first

--- app/src/main/java/com/example/first_cloud/MainActivity.kt
+++ app/src/main/java/com/example/first_cloud/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var name = "tahany1";
+        var name = "tahany11";
     }
 }
